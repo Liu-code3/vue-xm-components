419de6a8f7a39f2eeded7534d57c112e2d33c724
lxm <1944754630@qq.com>
2025-03-03 17:59:58 +0800

    fix(init)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { UserConfig } from 'vite'
+import { resolve } from 'path'
+
 import vue from '@vitejs/plugin-vue'
 
+// 当前执行node命令时文件夹的地址(工作目录)
+const root = process.cwd()
+// 路径查找
+function pathResolve(dir: string) {
+  return resolve(root, '.', dir)
+}
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default (): UserConfig => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: [
+        {
+          find: /@\//,
+          replacement: pathResolve('src') + '/',
+        },
+      ],
+    }
+  }
+}
